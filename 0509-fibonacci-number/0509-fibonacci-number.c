18db4008595d5c1a1b2bf9b7aad2053a2d7d6ba5
Nikita <patidarnikku13@gmail.com>
2026-03-12 00:34:38 +0530

    Time: 4 ms (34.69%), Space: 8.7 MB (5.97%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -0,0 +1,14 @@
+
+
+int fib(int n){
+    if (n == 0) return 0;
+    if  (n == 1) return 1;
+     int a=0,b=1;
+    for(int i = 2 ; i<=n;i++){
+        int fib = a+b;
+        a = b;
+        b = fib;
+    }
+    return b;
+
+}
